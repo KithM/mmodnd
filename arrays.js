cc974591950e122f6e1765968074cf33f28a779c
Kith M <malachite.akin@gmail.com>
2023-09-21 10:07:13 -0500

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -1,3 +1,113 @@
+const clothenchants = [
+    {p:1.00}
+];
+const leatherenchants = [
+    {p:1.00}
+];
+const metalenchants = [
+    {p:1.00}
+];
+const fiberenchants = [
+    {p:1.00}
+];
+const platedenchants = [
+    {p:1.00}
+];
+
+const lootTypes = [
+    { name: `Cloth`,               enchants: [{p:1.00}],       p: 0.35, level: 1 },//m: 0.250,  level: 1 },
+    { name: `Hide`,                enchants: [{p:1.00}],       p: 0.35, level: 1 },
+    { name: `Wood`,                enchants: [{p:1.00}],       p: 0.35, level: 1 },
+    { name: `Mail`,                enchants: metalenchants,    p: 0.35, level: 1 },
+    
+    { name: `Silk`,                enchants: clothenchants,    p: 0.35, level: 5 },
+    { name: `Leather`,             enchants: leatherenchants,  p: 0.35, level: 5 },
+    { name: `Iron`,                enchants: metalenchants,    p: 0.35, level: 5 },
+    { name: `Steel`,               enchants: metalenchants,    p: 0.30, level: 5 },
+    { name: `Steel Plate`,         enchants: metalenchants,    p: 0.25, level: 5 },
+
+    // Ring & Amulet Materials
+    { name: `Copper`,              enchants: metalenchants,    p: 0.15, level: 1},
+    { name: `Bronze`,              enchants: metalenchants,    p: 0.15, level: 1},
+    { name: `Brass`,               enchants: metalenchants,    p: 0.14, level: 3},
+    { name: `Silver`,              enchants: metalenchants,    p: 0.13, level: 5},
+    { name: `Amethyst`,            enchants: metalenchants,    p: 0.12, level: 10},
+    { name: `Aquamarine`,          enchants: metalenchants,    p: 0.11, level: 10},
+    { name: `Quartz`,              enchants: metalenchants,    p: 0.10, level: 15},
+    { name: `Gold`,                enchants: metalenchants,    p: 0.09, level: 15},
+    { name: `Jade`,                enchants: metalenchants,    p: 0.08, level: 15},
+    { name: `Platinum`,            enchants: metalenchants,    p: 0.07, level: 20},
+    { name: `Obsidian`,            enchants: metalenchants,    p: 0.06, level: 20},
+    { name: `Diamond`,             enchants: metalenchants,    p: 0.05, level: 25}
+];
+
+// Attributes
+const primaryAttributes = ['Strength', 'Intellect', 'Agility'];
+const secondaryAttributes = ['Stamina', 'Critical Strike', 'Regen'];
+
+const attributeProbabilities = {
+    'Stamina': 0.5,
+    'Critical Strike': 0.3,
+    'Regen': 0.2
+};
+
+// Item Names
+const itemNames = {
+    // Prefixes
+    'Hardened': [false, 'Stamina', 0.9],
+    'Wise': [false, 'Intellect', 0.9],
+    'Mighty': [false, 'Strength', 0.9],
+    'Swift': [false, 'Agility', 0.9],
+    'Tenacious': [false, ['Strength', 'Stamina'], 0.9],
+    'Vigorous': [false, ['Agility', 'Stamina'], 0.9],
+    'Arcane': [false, ['Intellect', 'Regen'], 0.9],    
+    'Unyielding': [false, ['Strength', 'Critical Strike'], 0.9],
+    'Eloquent': [false, ['Intellect', 'Critical Strike'], 0.9],
+    'Nimble': [false, ['Agility', 'Critical Strike'], 0.9],
+    'Restorative': [false, ['Stamina', 'Regen'], 0.9],
+
+    'Chromatic': [false, ['Strength','Agility','Intellect'], 0.9],
+    'Unstable': [false, ['Regen','Stamina','Critical Strike'], 0.9],
+
+    'Gladiator\'s': [false, ['Strength'], 0.9],
+    'Hero\'s': [false, ['Strength'], 0.9],
+    
+    // Suffixes
+    'of Strength': [true, ['Strength'], 0.9],
+    'of Intellect': [true, ['Intellect'], 0.9],
+    'of Agility': [true, ['Agility'], 0.9],
+
+    'of the Warrior': [true, ['Strength'], 0.9],
+    'of the Mage': [true, ['Intellect'], 0.9],
+    'of the Hunter': [true, ['Agility'], 0.9],
+
+    'of the Hero': [true, ['Strength'], 0.75],
+    'of Judgment': [true, ['Intellect', 'Regen'], 0.75],
+    'of Endurance': [true, 'Stamina', 0.75],
+    'of Fortitude': [true, ['Stamina', 'Strength'], 0.75],
+    'of Alacrity': [true, ['Agility', 'Critical Strike'], 0.75],
+    'of the Bear': [true, 'Strength', 0.75],
+    'of the Owl': [true, 'Intellect', 0.75],
+    'of the Lynx': [true, 'Agility', 0.75],
+    'of Precision': [true, 'Critical Strike', 0.75],
+    'of Restoration': [true, 'Regen', 0.75],
+    'of Resilience': [true, ['Stamina', 'Regen'], 0.75],
+    'of Insight': [true, ['Intellect', 'Critical Strike'], 0.75],
+    'of Finesse': [true, ['Agility', 'Regen'], 0.75],
+    'of the Gladiator': [true, ['Strength', 'Critical Strike'], 0.75],
+    'of Stability': [true, ['Stamina', 'Critical Strike'], 0.75],
+    'of the Sage': [true, ['Intellect', 'Stamina'], 0.75],
+    'of Ferocity': [true, ['Agility', 'Strength'], 0.75]
+};
+
+const itemQualities = [
+    { name: 'Common',       multiplier: 1.00, p: 0.55, applicableTo: ['Weapon', 'Equipment'] }, //black
+    { name: 'Uncommon',     multiplier: 1.50, p: 0.25, applicableTo: ['Weapon', 'Equipment'] }, //green
+    { name: 'Rare',         multiplier: 2.00, p: 0.1, applicableTo: ['Weapon', 'Equipment'] }, //blue
+    { name: 'Epic',         multiplier: 3.25, p: 0.08, applicableTo: ['Weapon', 'Equipment'] }, //purple
+    { name: 'Legendary',    multiplier: 5.00, p: 0.02, applicableTo: ['Weapon', 'Equipment'] } //orange
+];
+
 // Sample items array with base min and max damage and item slot
 const items = [
     // Weapons
@@ -128,114 +238,4 @@ const premadeItems = [
         description: 'Crafting Regeant (Tailoring)',
         type: 'Regeant'
     }
-];
-
-const clothenchants = [
-    {p:1.00}
-];
-const leatherenchants = [
-    {p:1.00}
-];
-const metalenchants = [
-    {p:1.00}
-];
-const fiberenchants = [
-    {p:1.00}
-];
-const platedenchants = [
-    {p:1.00}
-];
-
-const lootTypes = [
-    { name: `Cloth`,               enchants: [{p:1.00}],       p: 0.35, level: 1 },//m: 0.250,  level: 1 },
-    { name: `Hide`,                enchants: [{p:1.00}],       p: 0.35, level: 1 },
-    { name: `Wood`,                enchants: [{p:1.00}],       p: 0.35, level: 1 },
-    { name: `Mail`,                enchants: metalenchants,    p: 0.35, level: 1 },
-    
-    { name: `Silk`,                enchants: clothenchants,    p: 0.35, level: 5 },
-    { name: `Leather`,             enchants: leatherenchants,  p: 0.35, level: 5 },
-    { name: `Iron`,                enchants: metalenchants,    p: 0.35, level: 5 },
-    { name: `Steel`,               enchants: metalenchants,    p: 0.30, level: 5 },
-    { name: `Steel Plate`,         enchants: metalenchants,    p: 0.25, level: 5 },
-
-    // Ring & Amulet Materials
-    { name: `Copper`,              enchants: metalenchants,    p: 0.15, level: 1},
-    { name: `Bronze`,              enchants: metalenchants,    p: 0.15, level: 1},
-    { name: `Brass`,               enchants: metalenchants,    p: 0.14, level: 3},
-    { name: `Silver`,              enchants: metalenchants,    p: 0.13, level: 5},
-    { name: `Amethyst`,            enchants: metalenchants,    p: 0.12, level: 10},
-    { name: `Aquamarine`,          enchants: metalenchants,    p: 0.11, level: 10},
-    { name: `Quartz`,              enchants: metalenchants,    p: 0.10, level: 15},
-    { name: `Gold`,                enchants: metalenchants,    p: 0.09, level: 15},
-    { name: `Jade`,                enchants: metalenchants,    p: 0.08, level: 15},
-    { name: `Platinum`,            enchants: metalenchants,    p: 0.07, level: 20},
-    { name: `Obsidian`,            enchants: metalenchants,    p: 0.06, level: 20},
-    { name: `Diamond`,             enchants: metalenchants,    p: 0.05, level: 25}
-];
-
-// Attributes
-const primaryAttributes = ['Strength', 'Intellect', 'Agility'];
-const secondaryAttributes = ['Stamina', 'Critical Strike', 'Regen'];
-
-const attributeProbabilities = {
-    'Stamina': 0.5,
-    'Critical Strike': 0.3,
-    'Regen': 0.2
-};
-
-// Item Names
-const itemNames = {
-    // Prefixes
-    'Hardened': [false, 'Stamina', 0.9],
-    'Wise': [false, 'Intellect', 0.9],
-    'Mighty': [false, 'Strength', 0.9],
-    'Swift': [false, 'Agility', 0.9],
-    'Tenacious': [false, ['Strength', 'Stamina'], 0.9],
-    'Vigorous': [false, ['Agility', 'Stamina'], 0.9],
-    'Arcane': [false, ['Intellect', 'Regen'], 0.9],    
-    'Unyielding': [false, ['Strength', 'Critical Strike'], 0.9],
-    'Eloquent': [false, ['Intellect', 'Critical Strike'], 0.9],
-    'Nimble': [false, ['Agility', 'Critical Strike'], 0.9],
-    'Restorative': [false, ['Stamina', 'Regen'], 0.9],
-
-    'Chromatic': [false, ['Strength','Agility','Intellect'], 0.9],
-    'Unstable': [false, ['Regen','Stamina','Critical Strike'], 0.9],
-
-    'Gladiator\'s': [false, ['Strength'], 0.9],
-    'Hero\'s': [false, ['Strength'], 0.9],
-    
-    // Suffixes
-    'of Strength': [true, ['Strength'], 0.9],
-    'of Intellect': [true, ['Intellect'], 0.9],
-    'of Agility': [true, ['Agility'], 0.9],
-
-    'of the Warrior': [true, ['Strength'], 0.9],
-    'of the Mage': [true, ['Intellect'], 0.9],
-    'of the Hunter': [true, ['Agility'], 0.9],
-
-    'of the Hero': [true, ['Strength'], 0.75],
-    'of Judgment': [true, ['Intellect', 'Regen'], 0.75],
-    'of Endurance': [true, 'Stamina', 0.75],
-    'of Fortitude': [true, ['Stamina', 'Strength'], 0.75],
-    'of Alacrity': [true, ['Agility', 'Critical Strike'], 0.75],
-    'of the Bear': [true, 'Strength', 0.75],
-    'of the Owl': [true, 'Intellect', 0.75],
-    'of the Lynx': [true, 'Agility', 0.75],
-    'of Precision': [true, 'Critical Strike', 0.75],
-    'of Restoration': [true, 'Regen', 0.75],
-    'of Resilience': [true, ['Stamina', 'Regen'], 0.75],
-    'of Insight': [true, ['Intellect', 'Critical Strike'], 0.75],
-    'of Finesse': [true, ['Agility', 'Regen'], 0.75],
-    'of the Gladiator': [true, ['Strength', 'Critical Strike'], 0.75],
-    'of Stability': [true, ['Stamina', 'Critical Strike'], 0.75],
-    'of the Sage': [true, ['Intellect', 'Stamina'], 0.75],
-    'of Ferocity': [true, ['Agility', 'Strength'], 0.75]
-};
-
-const itemQualities = [
-    { name: 'Common',       multiplier: 1.00, p: 0.55, applicableTo: ['Weapon', 'Equipment'] }, //black
-    { name: 'Uncommon',     multiplier: 1.50, p: 0.25, applicableTo: ['Weapon', 'Equipment'] }, //green
-    { name: 'Rare',         multiplier: 2.00, p: 0.1, applicableTo: ['Weapon', 'Equipment'] }, //blue
-    { name: 'Epic',         multiplier: 3.25, p: 0.08, applicableTo: ['Weapon', 'Equipment'] }, //purple
-    { name: 'Legendary',    multiplier: 5.00, p: 0.02, applicableTo: ['Weapon', 'Equipment'] } //orange
 ];
